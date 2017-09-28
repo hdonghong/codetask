e6df59b21f2a1a51ba0efffab64d59cd0389010c
honhong <799108252@qq.com>
2017-09-28 23:07:00 +0800

    task 2, the first day

---
+++ task2/restdemo/src/java/per/dh/object/User.java
@@ -0,0 +1,41 @@
+package per.dh.object;
+
+public class User {
+    private String name;
+    private Integer age;
+    private ContactInfo contactInfo;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", contactInfo=" + contactInfo +
+                '}';
+    }
+
+    public ContactInfo getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(ContactInfo contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
